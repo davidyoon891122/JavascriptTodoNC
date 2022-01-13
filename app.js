bd388237c1d5cc188cb49ae3e18395e801925cb0
davidyoon <qqpo12@naver.com>
2022-01-13 20:42:17 +0900

    how to write variable in javascript

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 console.log("Hello JS");
 console.log(525252123);
+console.log("5" + 2);
+const number = 5;
+const secondNumber = 2;
+const myName = "DavidYoon"
+
+console.log(number + secondNumber);
+console.log(number / secondNumber);
+console.log(number * secondNumber);
+console.log("Hello " + myName);
