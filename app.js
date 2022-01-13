d463ebeb2b386d884a71b41f8e8dd5fbeb4e78e6
davidyoon <qqpo12@naver.com>
2022-01-13 20:52:51 +0900

    use const, let, var const is default -> can't change let -> can change var is old way -> can change

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 console.log("Hello JS");
 console.log(525252123);
 console.log("5" + 2);
-const number = 5;
-const secondNumber = 2;
-const myName = "DavidYoon"
+let number = 5;
+let secondNumber = 2;
+let myName = "DavidYoon"
+number = 3
 
 console.log(number + secondNumber);
 console.log(number / secondNumber);
 console.log(number * secondNumber);
 console.log("Hello " + myName);
+myName = "JiwonYoon"
+
+console.log("My new Name is " + myName);
+
+// const can't change, 
+// use const default
+// old way is var
+var varTest = 10;
+console.log(varTest);
+varTest = 5;
+console.log(varTest);
