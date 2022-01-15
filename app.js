7959ce49aa4e50351a6899e2b16acb2f3d84164a
davidyoon <qqpo12@naver.com>
2022-01-15 21:16:45 +0900

    use object

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+// before use object
+const playerName = "david";
+const playerPoints = 121212;
+const playerHandsome = false;
+const playerFat = "little bit"
 
-const daysOfWeek = [
-    mon,
-    tue,
-    wed,
-    thu,
-    fri,
-    sat,
-    sun,
-];
+// use array but can't understand meaning of values
+// player[0] == name...
+// const player = ["david", 121212, false, "little bit"];
 
-const nonsense = [1, 2, "Hello", false, null, undefined, ""];
 
-console.log(daysOfWeek[0], nonsense[1]);
-daysOfWeek.push("special");
-console.log(daysOfWeek);
+// object act like a dictionary
+const player = {
+    name: "david",
+    points: 121212,
+    handsome: false,
+    fat: "littel bit",
+};
+
+
+console.log(player.name);
+console.log(player["handsome"]);
+player.handsome = true;
+console.log(player.handsome); // object is still const but inside of object element can be modified
+player.lastName = "potato";
+console.log(player);
