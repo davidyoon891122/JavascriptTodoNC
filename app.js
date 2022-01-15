4b8463d51129788c4f4af7e926d3324e0e4b147a
davidyoon <qqpo12@naver.com>
2022-01-15 21:32:27 +0900

    make sayHello function add index.html to test alert()

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-// before use object
-const playerName = "david";
-const playerPoints = 121212;
-const playerHandsome = false;
-const playerFat = "little bit"
-
-// use array but can't understand meaning of values
-// player[0] == name...
-// const player = ["david", 121212, false, "little bit"];
-
-
-// object act like a dictionary
-const player = {
-    name: "david",
-    points: 121212,
-    handsome: false,
-    fat: "littel bit",
+function sayHello(name) {
+    console.log("Hello My name is" + name);
+    return "Hello My name is" + name;
 };
 
 
-console.log(player.name);
-console.log(player["handsome"]);
-player.handsome = true;
-console.log(player.handsome); // object is still const but inside of object element can be modified
-player.lastName = "potato";
-console.log(player);
+sayHello("david");
+sayHello("nico")
+sayHello("lynn");
+// alert(sayHello("david"));
