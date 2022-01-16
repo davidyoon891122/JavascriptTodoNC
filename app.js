2a697fcf50f872df1d2da79f89375d5f59d30ca7
davidyoon <qqpo12@naver.com>
2022-01-16 13:29:16 +0900

    return

--- app.js
+++ app.js
@@ -1,42 +1,18 @@
-// list, array
-const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-
-// object
-const player = {
-    name: "DavidYoon",
-    age: 34,
-    company: "KIS Securities&Investment Company"
-}
-//pring all functions in console
-// console.log(console);
-console.log(player);
-// add element
-player.sexy = true;
-console.log(player);
-
-//function
-
-function plus(a, b) {
-    return a + b
-}
-
-console.log(plus(1,2));
-
 const calculator = {
     add: function(a,b) {
-        console.log(a+b);
+        return a + b
     },
     subtract: function(a,b) {
-        console.log(a - b);
+        return a - b
     },
     mutiply: function(a,b) {
-        console.log(a * b)
+        return a * b
     },
     divide: function(a,b) {
-        console.log(a/b);
+        return a / b
     },
     power: function(a,b) {
-        console.log(a ** b);
+        return a ** b
     }
 }
 
@@ -44,4 +20,7 @@ calculator.add(1,2);
 calculator.subtract(5,3);
 calculator.mutiply(2,5);
 calculator.divide(10,2);
-calculator.power(3,3);
+const result = calculator.power(3,3)
+console.log(result);
+const addResult = calculator.add(result, 10);
+console.log(addResult);
