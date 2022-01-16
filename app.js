0992d86e4d7a0774ea68fa2a43898ef215eca8a7
davidyoon <qqpo12@naver.com>
2022-01-16 13:16:49 +0900

    recap list, arry, objet, function

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
-// recap
-// types
-// string, number, boolean, null, NaN, undefined
-const a = NaN
-const b = null
-let c;
-console.log(a, b, c);
+// list, array
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-console.log(42/0);
-console.log(42/-0);
+// object
+const player = {
+    name: "DavidYoon",
+    age: 34,
+    company: "KIS Securities&Investment Company"
+}
+//pring all functions in console
+// console.log(console);
+console.log(player);
+// add element
+player.sexy = true;
+console.log(player);
 
-// BigInt
-const x = 2n ** 53n 
-const y = x + 1n
-console.log(x, y);
+//function
 
-// Array
-const days = [1,2,"수", false, null, undefined];
-const toBuy = ["potato", "tomato", "pizza"]
-console.log(toBuy[2]);
-// update 
-toBuy[2] = "water"
+function plus(a, b) {
+    return a + b
+}
 
-console.log(toBuy[2]);
-// add
-toBuy.push("coke");
-console.log(toBuy);
+console.log(plus(1,2));
+
+const calculator = {
+    add: function(a,b) {
+        console.log(a+b);
+    },
+    subtract: function(a,b) {
+        console.log(a - b);
+    },
+    mutiply: function(a,b) {
+        console.log(a * b)
+    },
+    divide: function(a,b) {
+        console.log(a/b);
+    },
+    power: function(a,b) {
+        console.log(a ** b);
+    }
+}
+
+calculator.add(1,2);
+calculator.subtract(5,3);
+calculator.mutiply(2,5);
+calculator.divide(10,2);
+calculator.power(3,3);
