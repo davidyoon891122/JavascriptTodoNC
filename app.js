e3e0b9a5e0c26dbfa7126588efa2fde95beaf4aa
davidyoon <qqpo12@naver.com>
2022-01-17 20:26:41 +0900

    use document function to get data from html using js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const title = document.getElementById("title");
-title.innerText = "Got you!";
-console.log(title.id);
-console.log(title.className);
+// const hellos = document.getElementsByClassName("hello");
+// console.log(hellos);
+
+// const title = document.getElementsByTagName("h1");
+// console.log(title)
+
+const title = document.querySelector(".hello h1"); //.className tag return just one result(first element)
+const titles = document.querySelectorAll(".hello h2");
+const content = document.querySelector("#content");
+
+
+console.dir(content.innerText);
+console.log(titles);
+console.log(title);
