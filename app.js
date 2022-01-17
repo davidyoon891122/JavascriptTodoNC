7386c6575982b0f966831ec0b36444d0274d9282
davidyoon <qqpo12@naver.com>
2022-01-17 20:56:19 +0900

    add click event on title(h1) with handleTitleClick()

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// const hellos = document.getElementsByClassName("hello");
-// console.log(hellos);
-
-// const title = document.getElementsByTagName("h1");
-// console.log(title)
-
-const title = document.querySelector(".hello h1"); //.className tag return just one result(first element)
-const titles = document.querySelectorAll(".hello h2");
-const content = document.querySelector("#content");
+const title = document.querySelector(".hello:first-child h1");
 
 
-console.dir(content.innerText);
-console.log(titles);
-console.log(title);
+function handleTitleClick() {
+    //console.log("title was clicked!");
+    title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
+
