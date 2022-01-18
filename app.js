d99695b32492138e7f9c9edadaa7cbfaac72301a
davidyoon <qqpo12@naver.com>
2022-01-18 20:32:56 +0900

    add sample how to use toggle

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const h1 = document.querySelector(".hello:first-child h1");
 
 
 function handleClickEvent(){
-    const clickedClass = "clicked";
-    if (h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked"); 
+    /* 
+    if clicked is in classList, toggle will remove it, 
+    if there is not clicekd in className, toggle will add clicked
+    */
 }
 
 
