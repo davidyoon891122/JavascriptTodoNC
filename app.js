2f9c8217b52869113a1ecb8eb8d9ab05e30638e6
davidyoon <qqpo12@naver.com>
2022-01-24 20:16:56 +0900

    add login-form and get date using js

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-const h1 = document.querySelector(".hello:first-child h1");
-
-
-
-
-function handleClickEvent(){
-    h1.classList.toggle("clicked"); 
-    /* 
-    if clicked is in classList, toggle will remove it, 
-    if there is not clicekd in className, toggle will add clicked
-    */
-}
-
-
-h1.addEventListener("click", handleClickEvent);
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
 
