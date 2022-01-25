f6e88c67842b5a4586b6ab59da6d451e3ec9f60e
davidyoon <qqpo12@naver.com>
2022-01-25 19:16:48 +0900

    prevent link event with a tag

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginButton = document.querySelector("#login-form button");
+const link = document.querySelector("a");
 
-
-function onLoginSubmit(tomato) {
-    tomato.preventDefault();
-    console.log(tomato);
+function onLoginSubmit(event) {
+    event.preventDefault(); // stop default behavior
+    console.log(event);
     
 }
 
+function handleLinkClick(event) {
+    console.log(event)
+    event.preventDefault();
+    // alert("click")
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
