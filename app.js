1bfd3e21694ed931624def2a61d88d2252065978
davidyoon <qqpo12@naver.com>
2022-01-25 20:42:26 +0900

    add if condition for add or remove hidden class with localStorage

--- app.js
+++ app.js
@@ -2,15 +2,34 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+const USERNAME_KEY = "username";
 const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault(); // stop default behavior
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const userName = loginInput.value;
-    localStorage.setItem("userName", userName);
-    greeting.innerText = `Hello ${userName}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, userName);
+    printGreetings(userName);
 
 }
-loginForm.addEventListener("submit", onLoginSubmit);
+
+function printGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+console.log(savedUserName);
+
+if (savedUserName === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the name
+    printGreetings(savedUserName);
+
+}
