ee70e87361d3688ab16e6edcc28d76e547c9beed
davidyoon <qqpo12@naver.com>
2022-02-08 16:29:43 +0900

    crate li, span element and input newToDo and append it to toDoList(ul)

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-function handleToDoSubmit() {
+function paintTodo(newTodo) {
+    console.log(`i will paint ${newTodo}`);
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintTodo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
