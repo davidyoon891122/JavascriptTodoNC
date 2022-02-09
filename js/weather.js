7c4de720219dee275b62e09b8233c7fff27a99cb
davidyoon <qqpo12@naver.com>
2022-02-09 15:27:16 +0900

    add fetch weather api url and set data on span

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "767cd7ad6286d493b227a37032a0bcd6"
+
 function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log(lat, lng); 
+    const option = "metric"
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=${option}`
+    fetch(url).then(response => response.json()).then(data => {
+        console.log(data.name, data.weather[0].main);
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+
+        weather.innerText = data.name;;
+        city.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+
 }
 
 function onGeoError() {
